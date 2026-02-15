cae1cd85b27b33a447c62ae9af0307b5b39ba622
Devi Manikanta <manikantapalaparthi@gmail.com>
2026-02-16 01:00:15 +0530

    fix config

---
+++ src/database/config/config.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "localhost",
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.PROD_DB_USER,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+};
